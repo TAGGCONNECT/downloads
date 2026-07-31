c0727f9245d69657c429699a8f5d19dc09c2966e
TAGGCONNECT <rodrigosilva007@hotmail.com>
2026-07-31 12:35:14 -0300

    Update WiFi credentials and improve copy feedback

--- wifi/script.js
+++ wifi/script.js
@@ -1,19 +1,33 @@
-const wifi = {
+const wifi={
 
-rede:"BARBEARIA-PRIME",
+rede:"CAFE_AROMA",
 
-senha:"Barba2026@"
+senha:"Cafe2026@"
 
 };
 
 document.getElementById("rede").innerText=wifi.rede;
 
-document.getElementById("senha").value=wifi.senha;
+document.getElementById("senha").innerText=wifi.senha;
 
 function copiarSenha(){
 
 navigator.clipboard.writeText(wifi.senha);
 
-alert("Senha copiada!");
+const mensagem=document.getElementById("mensagem");
+
+const botao=document.getElementById("copiar");
+
+botao.innerHTML='<i class="fa-solid fa-check"></i>';
+
+mensagem.innerText="Senha copiada!";
+
+setTimeout(()=>{
+
+botao.innerHTML='<i class="fa-regular fa-copy"></i>';
+
+mensagem.innerText="";
+
+},2000);
 
 }
