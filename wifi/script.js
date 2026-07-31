370e5cd8b3f48b247c8c9661b63cf65d2fe6fea9
TAGGCONNECT <rodrigosilva007@hotmail.com>
2026-07-31 13:15:54 -0300

    Update Wi-Fi credentials and improve copy function

--- wifi/script.js
+++ wifi/script.js
@@ -1,24 +1,24 @@
-const wifi={
+const wifi = {
 
-rede:"BARBEARIA_PRIME",
+    rede: "CAFE_AROMA",
 
-senha:"Barba2026@"
+    senha: "Cafe2026@"
 
 };
 
-document.getElementById("rede").innerText=wifi.rede;
+document.getElementById("rede").innerText = wifi.rede;
+document.getElementById("senha").innerText = wifi.senha;
 
-document.getElementById("senha").innerText=wifi.senha;
+function copiarSenha() {
 
-function copiarSenha(){
+    navigator.clipboard.writeText(wifi.senha);
 
-navigator.clipboard.writeText(wifi.senha);
+    const mensagem = document.getElementById("mensagem");
+    const botao = document.getElementById("copiar");
 
-const mensagem=document.getElementById("mensagem");
+    botao.innerHTML = '<i class="fa-solid fa-check"></i>';
 
-const botao=document.getElementById("copiar");
+    mensagem.innerHTML =
+    "✅ Senha copiada!<br>Agora abra as configurações de Wi-Fi do seu celular e conecte-se à rede.";
 
-botao.innerHTML='<i class="fa-solid fa-check"></i>';
-
-mensagem.innerText="Senha copiada! Agora abra as configurações de Wi-Fi do seu celular para conectar.";
-
+}
