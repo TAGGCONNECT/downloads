29525b1ae9b7a51e0202c92029c51b9922a2db72
TAGGCONNECT <rodrigosilva007@hotmail.com>
2026-08-04 22:52:46 -0300

    Add Wi-Fi credentials and copy functionality

--- Dprint/Wifi/script.js
+++ Dprint/Wifi/script.js
@@ -1,1 +1,25 @@
+const wifi = {
+
+    rede: "\D Print",
+
+    senha: "Clara2021@"
+
+};
+
+document.getElementById("rede").innerText = wifi.rede;
+document.getElementById("senha").innerText = wifi.senha;
+
+function copiarSenha() {
+
+    navigator.clipboard.writeText(wifi.senha);
+
+    const mensagem = document.getElementById("mensagem");
+    const botao = document.getElementById("copiar");
+
+    botao.innerHTML = '<i class="fa-solid fa-check"></i>';
+
+    mensagem.innerHTML =
+    "✅ Senha copiada!<br>Agora abra as configurações de Wi-Fi do seu celular e conecte-se à rede.";
+
+}
 
