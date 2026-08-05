86b4da3e1479ac008a07a5cee15e40987c9662ef
TAGGCONNECT <rodrigosilva007@hotmail.com>
2026-08-04 23:34:10 -0300

    Rename function and update clipboard functionality

--- Dprint/Wifi/script.js
+++ Dprint/Wifi/script.js
@@ -9,13 +9,14 @@ const wifi = {
 document.getElementById("rede").innerText = wifi.rede;
 document.getElementById("senha").innerText = wifi.senha;
 
-function copiarPix(){
+function copiarSenha() {
 
-    const chave = document.getElementById("chavePix").innerText;
-
-    navigator.clipboard.writeText(chave);
+    navigator.clipboard.writeText(wifi.senha);
 
     const mensagem = document.getElementById("mensagem");
+    const botao = document.getElementById("copiar");
+
+    botao.innerHTML = '<i class="fa-solid fa-check"></i>';
 
     mensagem.innerHTML =
     "✅ Senha copiada!<br>Agora abra as configurações de Wi-Fi do seu celular e conecte-se à rede.";
